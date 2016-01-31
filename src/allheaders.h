DanBloomberg/leptonica

commit ca9d16af54074812331fbc08a76a0410f6955796

Change fmemopen behavior, windows temp files.

src/allheaders.h
@@ -190,7 +190,7 @@ LEPT_DLL extern PIXA * pixaGenerateFontFromString ( l_int32 fontsize, l_int32 *p
 LEPT_DLL extern PIXA * pixaGenerateFont ( PIX *pixs, l_int32 fontsize, l_int32 *pbl0, l_int32 *pbl1, l_int32 *pbl2 );
 LEPT_DLL extern PIX * pixReadStreamBmp ( FILE *fp );
 LEPT_DLL extern l_int32 pixWriteStreamBmp ( FILE *fp, PIX *pix );
-LEPT_DLL extern PIX * pixReadMemBmp ( const l_uint8 *cdata, size_t size );
+LEPT_DLL extern PIX * pixReadMemBmp ( const l_uint8 *data, size_t size );
 LEPT_DLL extern l_int32 pixWriteMemBmp ( l_uint8 **pdata, size_t *psize, PIX *pix );
 LEPT_DLL extern void * l_bootnum_gen1 ( void );
 LEPT_DLL extern void * l_bootnum_gen2 ( void );
@@ -1862,15 +1862,15 @@ LEPT_DLL extern l_int32 pixWritePng ( const char *filename, PIX *pix, l_float32 
 LEPT_DLL extern l_int32 pixWriteStreamPng ( FILE *fp, PIX *pix, l_float32 gamma );
 LEPT_DLL extern l_int32 pixSetZlibCompression ( PIX *pix, l_int32 compval );
 LEPT_DLL extern void l_pngSetReadStrip16To8 ( l_int32 flag );
-LEPT_DLL extern PIX * pixReadMemPng ( const l_uint8 *cdata, size_t size );
+LEPT_DLL extern PIX * pixReadMemPng ( const l_uint8 *data, size_t size );
 LEPT_DLL extern l_int32 pixWriteMemPng ( l_uint8 **pdata, size_t *psize, PIX *pix, l_float32 gamma );
 LEPT_DLL extern PIX * pixReadStreamPnm ( FILE *fp );
 LEPT_DLL extern l_int32 readHeaderPnm ( const char *filename, l_int32 *pw, l_int32 *ph, l_int32 *pd, l_int32 *ptype, l_int32 *pbps, l_int32 *pspp );
 LEPT_DLL extern l_int32 freadHeaderPnm ( FILE *fp, l_int32 *pw, l_int32 *ph, l_int32 *pd, l_int32 *ptype, l_int32 *pbps, l_int32 *pspp );
 LEPT_DLL extern l_int32 pixWriteStreamPnm ( FILE *fp, PIX *pix );
 LEPT_DLL extern l_int32 pixWriteStreamAsciiPnm ( FILE *fp, PIX *pix );
-LEPT_DLL extern PIX * pixReadMemPnm ( const l_uint8 *cdata, size_t size );
-LEPT_DLL extern l_int32 readHeaderMemPnm ( const l_uint8 *cdata, size_t size, l_int32 *pw, l_int32 *ph, l_int32 *pd, l_int32 *ptype, l_int32 *pbps, l_int32 *pspp );
+LEPT_DLL extern PIX * pixReadMemPnm ( const l_uint8 *data, size_t size );
+LEPT_DLL extern l_int32 readHeaderMemPnm ( const l_uint8 *data, size_t size, l_int32 *pw, l_int32 *ph, l_int32 *pd, l_int32 *ptype, l_int32 *pbps, l_int32 *pspp );
 LEPT_DLL extern l_int32 pixWriteMemPnm ( l_uint8 **pdata, size_t *psize, PIX *pix );
 LEPT_DLL extern PIX * pixProjectiveSampledPta ( PIX *pixs, PTA *ptad, PTA *ptas, l_int32 incolor );
 LEPT_DLL extern PIX * pixProjectiveSampled ( PIX *pixs, l_float32 *vc, l_int32 incolor );
@@ -2536,6 +2536,8 @@ LEPT_DLL extern l_uint32 convertOnLittleEnd32 ( l_uint32 wordin );
 LEPT_DLL extern l_uint32 convertOnBigEnd32 ( l_uint32 wordin );
 LEPT_DLL extern FILE * fopenReadStream ( const char *filename );
 LEPT_DLL extern FILE * fopenWriteStream ( const char *filename, const char *modestring );
+LEPT_DLL extern FILE * fopenReadFromMemory ( const l_uint8 *data, size_t size );
+LEPT_DLL extern FILE * fopenWriteWinTmpfile (  );
 LEPT_DLL extern FILE * lept_fopen ( const char *filename, const char *mode );
 LEPT_DLL extern l_int32 lept_fclose ( FILE *fp );
 LEPT_DLL extern void * lept_calloc ( size_t nmemb, size_t size );

src/utils.c
@@ -88,6 +88,10 @@
  *       Cross-platform functions for opening file streams
  *           FILE      *fopenReadStream()
  *           FILE      *fopenWriteStream()
+ *           FILE      *fopenReadFromMemory()
+ *
+ *       Opening a windows tmpfile for writing
+ *           FILE      *fopenWriteWinTmpfile()
  *
  *       Cross-platform functions that avoid C-runtime boundary crossing
  *       with Windows DLLs
@@ -113,7 +117,7 @@
  *           char      *appendSubdirs()
  *
  *       Special file name operations
-  *          l_int32    convertSepCharsInPath()
+ *           l_int32    convertSepCharsInPath()
  *           char      *genPathname()
  *           l_int32    makeTempDirname()
  *           l_int32    modifyTrailingSlash()
@@ -1754,6 +1758,10 @@ convertOnBigEnd32(l_uint32  wordin)
 /*--------------------------------------------------------------------*
  *                        Opening file streams                        *
  *--------------------------------------------------------------------*/
+#if HAVE_FMEMOPEN
+extern FILE *fmemopen(void *data, size_t size, const char *mode);
+#endif  /* HAVE_FMEMOPEN */
+
 /*!
  *  fopenReadStream()
  *
@@ -1763,10 +1771,8 @@ convertOnBigEnd32(l_uint32  wordin)
  *  Notes:
  *      (1) This should be used whenever you want to run fopen() to
  *          read from a stream.  Never call fopen() directory.
- *      (2) This also handles pathname conversions, if necessary:
- *           ==>   /tmp              (unix)  [default]
- *           ==>   /tmp/leptonica    (unix)  [if ADD_LEPTONICA_SUBDIR == 1]
- *           ==>   <Temp>/leptonica  (windows)
+ *      (2) This handles the temp directory pathname conversion on windows:
+ *              /tmp  ==>  <Windows Temp directory>
  */
 FILE *
 fopenReadStream(const char  *filename)
@@ -1806,10 +1812,8 @@ FILE  *fp;
  *  Notes:
  *      (1) This should be used whenever you want to run fopen() to
  *          write or append to a stream.  Never call fopen() directory.
- *      (2) This also handles pathname conversions, if necessary:
- *           ==>   /tmp              (unix)  [default]
- *           ==>   /tmp/leptonica    (unix)  [if ADD_LEPTONICA_SUBDIR == 1]
- *           ==>   <Temp>/leptonica  (windows)
+ *      (2) This handles the temp directory pathname conversion on windows:
+ *              /tmp  ==>  <Windows Temp directory>
  */
 FILE *
 fopenWriteStream(const char  *filename,
@@ -1829,6 +1833,98 @@ FILE  *fp;
     if (!fp)
         return (FILE *)ERROR_PTR("stream not opened", procName, NULL);
     return fp;
+}
+
+
+/*!
+ *  fopenReadFromMemory()
+ *
+ *      Input:  data, size
+ *      Return: file stream, or null on error
+ *
+ *  Notes:
+ *      (1) Work-around if fmemopen() not available.
+ *      (2) Windows tmpfile() writes into the root C:\ directory, which
+ *          requires admin privileges.  This also works around that.
+ */
+FILE *
+fopenReadFromMemory(const l_uint8  *data,
+                    size_t          size)
+{
+#if defined(_WIN32)
+l_int32     handle;
+char       *tmpdir;
+char        filename[64];
+const char  fn_template[] = "mkstemp.XXXXXX";
+#endif  /* _WIN32 */
+FILE       *fp;
+
+    PROCNAME("fopenReadFromMemory");
+
+    if (!data)
+        return (FILE *)ERROR_PTR("data not defined", procName, NULL);
+
+#if HAVE_FMEMOPEN
+    if ((fp = fmemopen((void *)data, size, "rb")) == NULL)
+        return (FILE *)ERROR_PTR("stream not opened", procName, NULL);
+#else  /* write to tmp file */
+    L_WARNING("work-around: writing to a temp file\n", procName);
+  #ifdef _WIN32
+    tmpdir = getenv("TMP");
+    snprintf(filename, sizeof(filename), "%s/%s", tmpdir ? tmpdir : ".",
+             fn_template);
+    handle = mkstemp(filename);
+    if ((fp = fdopen(handle, "r+b")) == NULL) {
+        L_ERROR("mkstemp stream not opened, %s\n", procName, strerror(errno));
+        return NULL;
+    }
+  #else
+    if ((fp = tmpfile()) == NULL)
+        return (FILE *)ERROR_PTR("tmpfile stream not opened", procName, NULL);
+  #endif  /*  _WIN32 */
+    fwrite(data, 1, size, fp);
+    rewind(fp);
+#endif  /* HAVE_FMEMOPEN */
+
+    return fp;
+}
+
+
+/*--------------------------------------------------------------------*
+ *                Opening a windows tmpfile for writing               *
+ *--------------------------------------------------------------------*/
+/*!
+ *  fopenWriteWinTmpfile()
+ *
+ *      Return: file stream, or null on error
+ *
+ *  Notes:
+ *      (1) Windows tmpfile() writes into the root C:\ directory, which
+ *          requires admin privileges.  This provides an alternative
+ *          implementation.
+ */
+FILE *
+fopenWriteWinTmpfile()
+{
+#ifdef _WIN32
+l_int32     handle;
+char       *tmpdir;
+char        filename[64];
+const char  fn_template[] = "mkstemp.XXXXXX";
+FILE       *fp;
+
+    PROCNAME("fopenWriteWinTmpfile");
+
+    tmpdir = getenv("TMP");
+    snprintf(filename, sizeof(filename), "%s/%s", tmpdir ? tmpdir : ".",
+             fn_template);
+    handle = mkstemp(filename);
+    if ((fp = fdopen(handle, "w+b")) == NULL) {
+        L_ERROR("mkstemp stream not opened, %s\n", procName, strerror(errno));
+        return NULL;
+    }
+    return fp;
+#endif  /*  _WIN32 */
 }
 
 
@@ -3459,7 +3555,7 @@ l_uint64  limit, ratio;
         return 0;
     }
 
-    limit = sqrt(n);
+    limit = (l_uint64)sqrt((l_float64)n);
     for (div = 3; div < limit; div += 2) {
        ratio = n / div;
        if (ratio * div == n) {

src/bmpio.c
@@ -571,13 +571,12 @@ RGBA_QUAD  *pquad;
  *---------------------------------------------------------------------*/
 #if HAVE_FMEMOPEN
 extern FILE *open_memstream(char **data, size_t *size);
-extern FILE *fmemopen(void *data, size_t size, const char *mode);
 #endif  /* HAVE_FMEMOPEN */
 
 /*!
  *  pixReadMemBmp()
  *
- *      Input:  cdata (const; bmp-encoded)
+ *      Input:  data (const; bmp-encoded)
  *              size (of data)
  *      Return: pix, or null on error
  *
@@ -585,7 +584,7 @@ extern FILE *fmemopen(void *data, size_t size, const char *mode);
  *      (1) The @size byte of @data must be a null character.
  */
 PIX *
-pixReadMemBmp(const l_uint8  *cdata,
+pixReadMemBmp(const l_uint8  *data,
               size_t          size)
 {
 FILE  *fp;
@@ -593,19 +592,11 @@ PIX   *pix;
 
     PROCNAME("pixReadMemBmp");
 
-    if (!cdata)
-        return (PIX *)ERROR_PTR("cdata not defined", procName, NULL);
+    if (!data)
+        return (PIX *)ERROR_PTR("data not defined", procName, NULL);
 
-#if HAVE_FMEMOPEN
-    if ((fp = fmemopen((l_uint8 *)cdata, size, "rb")) == NULL)
+    if ((fp = fopenReadFromMemory(data, size)) == NULL)
         return (PIX *)ERROR_PTR("stream not opened", procName, NULL);
-#else
-    L_WARNING("work-around: writing to a temp file\n", procName);
-    if ((fp = tmpfile()) == NULL)
-        return (PIX *)ERROR_PTR("tmpfile stream not opened", procName, NULL);
-    fwrite(cdata, 1, size, fp);
-    rewind(fp);
-#endif  /* HAVE_FMEMOPEN */
     pix = pixReadStreamBmp(fp);
     fclose(fp);
     if (!pix) L_ERROR("pix not read\n", procName);
@@ -648,8 +639,13 @@ FILE    *fp;
     ret = pixWriteStreamBmp(fp, pix);
 #else
     L_WARNING("work-around: writing to a temp file\n", procName);
+  #ifdef _WIN32
+    if ((fp = fopenWriteWinTempfile()) == NULL)
+        return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #else
     if ((fp = tmpfile()) == NULL)
         return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #endif  /* _WIN32 */
     ret = pixWriteStreamBmp(fp, pix);
     rewind(fp);
     *pdata = l_binaryReadStream(fp, psize);
@@ -657,6 +653,7 @@ FILE    *fp;
     fclose(fp);
     return ret;
 }
+
 
 /* --------------------------------------------*/
 #endif  /* USE_BMPIO */

src/jp2kio.c
@@ -745,7 +745,6 @@ opj_image_cmptparm_t  cmptparm[4];
  *---------------------------------------------------------------------*/
 #if HAVE_FMEMOPEN
 extern FILE *open_memstream(char **data, size_t *size);
-extern FILE *fmemopen(void *data, size_t size, const char *mode);
 #endif  /* HAVE_FMEMOPEN */
 
 /*!
@@ -782,16 +781,8 @@ PIX      *pix;
     if (!data)
         return (PIX *)ERROR_PTR("data not defined", procName, NULL);
 
-#if HAVE_FMEMOPEN
-    if ((fp = fmemopen((void *)data, size, "rb")) == NULL)
+    if ((fp = fopenReadFromMemory(data, size)) == NULL)
         return (PIX *)ERROR_PTR("stream not opened", procName, NULL);
-#else
-    L_WARNING("work-around: writing to a temp file\n", procName);
-    if ((fp = tmpfile()) == NULL)
-        return (PIX *)ERROR_PTR("tmpfile stream not opened", procName, NULL);
-    fwrite(data, 1, size, fp);
-    rewind(fp);
-#endif  /* HAVE_FMEMOPEN */
     pix = pixReadStreamJp2k(fp, reduction, box, hint, debug);
     fclose(fp);
     if (!pix) L_ERROR("pix not read\n", procName);
@@ -844,8 +835,13 @@ FILE    *fp;
     ret = pixWriteStreamJp2k(fp, pix, quality, nlevels, hint, debug);
 #else
     L_WARNING("work-around: writing to a temp file\n", procName);
+  #ifdef _WIN32
+    if ((fp = fopenWriteWinTempfile()) == NULL)
+        return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #else
     if ((fp = tmpfile()) == NULL)
         return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #endif  /* _WIN32 */
     ret = pixWriteStreamJp2k(fp, pix, quality, nlevels, hint, debug);
     rewind(fp);
     *pdata = l_binaryReadStream(fp, psize);

src/jpegio.c
@@ -942,7 +942,6 @@ jmp_buf                      jmpbuf;  /* must be local to the function */
  *---------------------------------------------------------------------*/
 #if HAVE_FMEMOPEN
 extern FILE *open_memstream(char **data, size_t *size);
-extern FILE *fmemopen(void *data, size_t size, const char *mode);
 #endif  /* HAVE_FMEMOPEN */
 
 /*!
@@ -983,16 +982,8 @@ PIX      *pix;
     if (!data)
         return (PIX *)ERROR_PTR("data not defined", procName, NULL);
 
-#if HAVE_FMEMOPEN
-    if ((fp = fmemopen((l_uint8 *)data, size, "rb")) == NULL)
+    if ((fp = fopenReadFromMemory(data, size)) == NULL)
         return (PIX *)ERROR_PTR("stream not opened", procName, NULL);
-#else
-    L_WARNING("work-around: writing to a temp file\n", procName);
-    if ((fp = tmpfile()) == NULL)
-        return (PIX *)ERROR_PTR("tmpfile stream not opened", procName, NULL);
-    fwrite(data, 1, size, fp);
-    rewind(fp);
-#endif  /* HAVE_FMEMOPEN */
     pix = pixReadStreamJpeg(fp, cmflag, reduction, pnwarn, hint);
     if (pix) {
         ret = fgetJpegComment(fp, &comment);
@@ -1043,16 +1034,8 @@ FILE    *fp;
     if (!pw && !ph && !pspp && !pycck && !pcmyk)
         return ERROR_INT("no results requested", procName, 1);
 
-#if HAVE_FMEMOPEN
-    if ((fp = fmemopen((l_uint8 *)data, size, "rb")) == NULL)
+    if ((fp = fopenReadFromMemory(data, size)) == NULL)
         return ERROR_INT("stream not opened", procName, 1);
-#else
-    L_WARNING("work-around: writing to a temp file\n", procName);
-    if ((fp = tmpfile()) == NULL)
-        return ERROR_INT("tmpfile stream not opened", procName, 1);
-    fwrite(data, 1, size, fp);
-    rewind(fp);
-#endif  /* HAVE_FMEMOPEN */
     ret = freadHeaderJpeg(fp, pw, ph, pspp, pycck, pcmyk);
     fclose(fp);
     return ret;
@@ -1100,8 +1083,13 @@ FILE    *fp;
     ret = pixWriteStreamJpeg(fp, pix, quality, progressive);
 #else
     L_WARNING("work-around: writing to a temp file\n", procName);
+  #ifdef _WIN32
+    if ((fp = fopenWriteWinTempfile()) == NULL)
+        return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #else
     if ((fp = tmpfile()) == NULL)
         return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #endif  /* _WIN32 */
     ret = pixWriteStreamJpeg(fp, pix, quality, progressive);
     rewind(fp);
     *pdata = l_binaryReadStream(fp, psize);

src/pngio.c
@@ -1256,13 +1256,12 @@ l_pngSetReadStrip16To8(l_int32  flag)
  *---------------------------------------------------------------------*/
 #if HAVE_FMEMOPEN
 extern FILE *open_memstream(char **data, size_t *size);
-extern FILE *fmemopen(void *data, size_t size, const char *mode);
 #endif  /* HAVE_FMEMOPEN */
 
 /*!
  *  pixReadMemPng()
  *
- *      Input:  cdata (const; png-encoded)
+ *      Input:  data (const; png-encoded)
  *              size (of data)
  *      Return: pix, or null on error
  *
@@ -1270,7 +1269,7 @@ extern FILE *fmemopen(void *data, size_t size, const char *mode);
  *      (1) The @size byte of @data must be a null character.
  */
 PIX *
-pixReadMemPng(const l_uint8  *cdata,
+pixReadMemPng(const l_uint8  *data,
               size_t          size)
 {
 FILE  *fp;
@@ -1278,19 +1277,11 @@ PIX   *pix;
 
     PROCNAME("pixReadMemPng");
 
-    if (!cdata)
+    if (!data)
         return (PIX *)ERROR_PTR("cdata not defined", procName, NULL);
 
-#if HAVE_FMEMOPEN
-    if ((fp = fmemopen((void *)cdata, size, "rb")) == NULL)
+    if ((fp = fopenReadFromMemory(data, size)) == NULL)
         return (PIX *)ERROR_PTR("stream not opened", procName, NULL);
-#else
-    L_WARNING("work-around: writing to a temp file\n", procName);
-    if ((fp = tmpfile()) == NULL)
-        return (PIX *)ERROR_PTR("tmpfile stream not opened", procName, NULL);
-    fwrite(cdata, 1, size, fp);
-    rewind(fp);
-#endif  /* HAVE_FMEMOPEN */
     pix = pixReadStreamPng(fp);
     fclose(fp);
     if (!pix) L_ERROR("pix not read\n", procName);
@@ -1337,8 +1328,13 @@ FILE    *fp;
     ret = pixWriteStreamPng(fp, pix, gamma);
 #else
     L_WARNING("work-around: writing to a temp file\n", procName);
+  #ifdef _WIN32
+    if ((fp = fopenWriteWinTempfile()) == NULL)
+        return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #else
     if ((fp = tmpfile()) == NULL)
         return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #endif  /* _WIN32 */
     ret = pixWriteStreamPng(fp, pix, gamma);
     rewind(fp);
     *pdata = l_binaryReadStream(fp, psize);

src/pnmio.c
@@ -600,13 +600,12 @@ PIX       *pixs;
  *---------------------------------------------------------------------*/
 #if HAVE_FMEMOPEN
 extern FILE *open_memstream(char **data, size_t *size);
-extern FILE *fmemopen(void *data, size_t size, const char *mode);
 #endif  /* HAVE_FMEMOPEN */
 
 /*!
  *  pixReadMemPnm()
  *
- *      Input:  cdata (const; pnm-encoded)
+ *      Input:  data (const; pnm-encoded)
  *              size (of data)
  *      Return: pix, or null on error
  *
@@ -614,7 +613,7 @@ extern FILE *fmemopen(void *data, size_t size, const char *mode);
  *      (1) The @size byte of @data must be a null character.
  */
 PIX *
-pixReadMemPnm(const l_uint8  *cdata,
+pixReadMemPnm(const l_uint8  *data,
               size_t          size)
 {
 FILE  *fp;
@@ -622,19 +621,10 @@ PIX   *pix;
 
     PROCNAME("pixReadMemPnm");
 
-    if (!cdata)
-        return (PIX *)ERROR_PTR("cdata not defined", procName, NULL);
-
-#if HAVE_FMEMOPEN
-    if ((fp = fmemopen((l_uint8 *)cdata, size, "rb")) == NULL)
+    if (!data)
+        return (PIX *)ERROR_PTR("data not defined", procName, NULL);
+    if ((fp = fopenReadFromMemory(data, size)) == NULL)
         return (PIX *)ERROR_PTR("stream not opened", procName, NULL);
-#else
-    L_WARNING("work-around: writing to a temp file\n", procName);
-    if ((fp = tmpfile()) == NULL)
-        return (PIX *)ERROR_PTR("tmpfile stream not opened", procName, NULL);
-    fwrite(cdata, 1, size, fp);
-    rewind(fp);
-#endif  /* HAVE_FMEMOPEN */
     pix = pixReadStreamPnm(fp);
     fclose(fp);
     if (!pix) L_ERROR("pix not read\n", procName);
@@ -645,7 +635,7 @@ PIX   *pix;
 /*!
  *  readHeaderMemPnm()
  *
- *      Input:  cdata (const; pnm-encoded)
+ *      Input:  data (const; pnm-encoded)
  *              size (of data)
  *              &w (<optional return>)
  *              &h (<optional return>)
@@ -656,7 +646,7 @@ PIX   *pix;
  *      Return: 0 if OK, 1 on error
  */
 l_int32
-readHeaderMemPnm(const l_uint8  *cdata,
+readHeaderMemPnm(const l_uint8  *data,
                  size_t          size,
                  l_int32        *pw,
                  l_int32        *ph,
@@ -670,19 +660,11 @@ FILE    *fp;
 
     PROCNAME("readHeaderMemPnm");
 
-    if (!cdata)
-        return ERROR_INT("cdata not defined", procName, 1);
+    if (!data)
+        return ERROR_INT("data not defined", procName, 1);
 
-#if HAVE_FMEMOPEN
-    if ((fp = fmemopen((l_uint8 *)cdata, size, "rb")) == NULL)
+    if ((fp = fopenReadFromMemory(data, size)) == NULL)
         return ERROR_INT("stream not opened", procName, 1);
-#else
-    L_WARNING("work-around: writing to a temp file\n", procName);
-    if ((fp = tmpfile()) == NULL)
-        return ERROR_INT("tmpfile stream not opened", procName, 1);
-    fwrite(cdata, 1, size, fp);
-    rewind(fp);
-#endif  /* HAVE_FMEMOPEN */
     ret = freadHeaderPnm(fp, pw, ph, pd, ptype, pbps, pspp);
     fclose(fp);
     if (ret)
@@ -728,8 +710,13 @@ FILE    *fp;
     ret = pixWriteStreamPnm(fp, pix);
 #else
     L_WARNING("work-around: writing to a temp file\n", procName);
+  #ifdef _WIN32
+    if ((fp = fopenWriteWinTempfile()) == NULL)
+        return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #else
     if ((fp = tmpfile()) == NULL)
         return ERROR_INT("tmpfile stream not opened", procName, 1);
+  #endif  /* _WIN32 */
     ret = pixWriteStreamPnm(fp, pix);
     rewind(fp);
     *pdata = l_binaryReadStream(fp, psize);

commit df978104c73e485cc4b528f7aadf6c60c4360fd0

Fix typo in function name (regression for Windows builds)
Commit ca9d16af54074812331fbc08a76a0410f6955796 introduced
a new function fopenWriteWinTempfile, but used
fopenWriteWinTmpfile for the implementation.

This caused builds for Windows to fail.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

src/allheaders.h
@@ -2537,7 +2537,7 @@ LEPT_DLL extern l_uint32 convertOnBigEnd32 ( l_uint32 wordin );
 LEPT_DLL extern FILE * fopenReadStream ( const char *filename );
 LEPT_DLL extern FILE * fopenWriteStream ( const char *filename, const char *modestring );
 LEPT_DLL extern FILE * fopenReadFromMemory ( const l_uint8 *data, size_t size );
-LEPT_DLL extern FILE * fopenWriteWinTmpfile (  );
+LEPT_DLL extern FILE * fopenWriteWinTempfile (  );
 LEPT_DLL extern FILE * lept_fopen ( const char *filename, const char *mode );
 LEPT_DLL extern l_int32 lept_fclose ( FILE *fp );
 LEPT_DLL extern void * lept_calloc ( size_t nmemb, size_t size );

src/utils.c
@@ -91,7 +91,7 @@
  *           FILE      *fopenReadFromMemory()
  *
  *       Opening a windows tmpfile for writing
- *           FILE      *fopenWriteWinTmpfile()
+ *           FILE      *fopenWriteWinTempfile()
  *
  *       Cross-platform functions that avoid C-runtime boundary crossing
  *       with Windows DLLs
@@ -1894,7 +1894,7 @@ FILE       *fp;
  *                Opening a windows tmpfile for writing               *
  *--------------------------------------------------------------------*/
 /*!
- *  fopenWriteWinTmpfile()
+ *  fopenWriteWinTempfile()
  *
  *      Return: file stream, or null on error
  *
@@ -1904,7 +1904,7 @@ FILE       *fp;
  *          implementation.
  */
 FILE *
-fopenWriteWinTmpfile()
+fopenWriteWinTempfile()
 {
 #ifdef _WIN32
 l_int32     handle;
@@ -1913,7 +1913,7 @@ char        filename[64];
 const char  fn_template[] = "mkstemp.XXXXXX";
 FILE       *fp;
 
-    PROCNAME("fopenWriteWinTmpfile");
+    PROCNAME("fopenWriteWinTempfile");
 
     tmpdir = getenv("TMP");
     snprintf(filename, sizeof(filename), "%s/%s", tmpdir ? tmpdir : ".",