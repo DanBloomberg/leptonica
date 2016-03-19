a722a2e85d6629ebdf111536c564a0b710dce61c
Dan Bloomberg <dan.bloomberg@gmail.com>
2016-03-18 18:13:28 -0700

    Clean up some programs that use gplot with X11
     * numa1_reg.c and numa2_reg.c: removed sleep; use tiled output
     * pixa1_reg.c and rank_reg.c: removed gplot calls with X11;
       do not use gthumb; make composite images of results;

--- prog/rank_reg.c
+++ prog/rank_reg.c
@@ -45,12 +45,14 @@ l_int32       i, j, w, h, same;
 l_float32     t, t1, t2;
 GPLOT        *gplot;
 NUMA         *nax, *nay1, *nay2;
-PIX          *pixs, *pixd, *pixt1, *pixt2, *pixt3, *pixt4;
+PIX          *pixs, *pixd, *pix1, *pix2, *pix3, *pix4;
 PIXA         *pixa;
 static char   mainName[] = "rank_reg";
 
     if (argc != 1)
         return ERROR_INT(" Syntax: rank_reg", mainName, 1);
+
+    lept_mkdir("lept/rank");
 
     if ((pixs = pixRead("lucasta.150.jpg")) == NULL)
         return ERROR_INT("pixs not made", mainName, 1);
@@ -63,62 +65,67 @@ static char   mainName[] = "rank_reg";
     fprintf(stderr, "MPix/sec: %7.3f\n", 0.000001 * w * h / t);
     pixDisplay(pixs, 0, 200);
     pixDisplay(pixd, 600, 200);
-    pixWrite("/tmp/filter.png", pixd, IFF_PNG);
+    pixWrite("/tmp/lept/rank/filter.png", pixd, IFF_PNG);
     pixDestroy(&pixd);
 
+
+    /* ---------- Compare grayscale morph with rank operator ---------- */
         /* Get results for dilation */
     startTimer();
-    pixt1 = pixDilateGray(pixs, 15, 15);
+    pix1 = pixDilateGray(pixs, 15, 15);
     t = stopTimer();
     fprintf(stderr, "Dilation time =  %7.3f sec\n", t);
 
         /* Get results for erosion */
-    pixt2 = pixErodeGray(pixs, 15, 15);
+    pix2 = pixErodeGray(pixs, 15, 15);
 
         /* Get results using the rank filter for rank = 0.0 and 1.0.
          * Don't use 0.0 or 1.0, because those are dispatched
          * automatically to erosion and dilation! */
-    pixt3 = pixRankFilterGray(pixs, 15, 15, 0.0001);
-    pixt4 = pixRankFilterGray(pixs, 15, 15, 0.9999);
+    pix3 = pixRankFilterGray(pixs, 15, 15, 0.0001);
+    pix4 = pixRankFilterGray(pixs, 15, 15, 0.9999);
 
         /* Compare */
-    pixEqual(pixt1, pixt4, &same);
+    pixEqual(pix1, pix4, &same);
     if (same)
         fprintf(stderr, "Correct: dilation results same as rank 1.0\n");
     else
         fprintf(stderr, "Error: dilation results differ from rank 1.0\n");
-    pixEqual(pixt2, pixt3, &same);
+    pixEqual(pix2, pix3, &same);
     if (same)
         fprintf(stderr, "Correct: erosion results same as rank 0.0\n");
     else
         fprintf(stderr, "Error: erosion results differ from rank 0.0\n");
-    pixDestroy(&pixt1);
-    pixDestroy(&pixt2);
-    pixDestroy(&pixt3);
-    pixDestroy(&pixt4);
+    pixDestroy(&pix1);
+    pixDestroy(&pix2);
+    pixDestroy(&pix3);
+    pixDestroy(&pix4);
 
+
+    /* ------------- Timing and filter size experiments --------- */
     fprintf(stderr, "\n----------------------------------------\n");
-    fprintf(stderr, "The next part takes about 30 seconds\n");
+    fprintf(stderr, "The next part takes about 10 seconds\n");
     fprintf(stderr, "----------------------------------------\n\n");
 
     nax = numaMakeSequence(1, 1, SIZE);
     nay1 = numaCreate(SIZE);
     nay2 = numaCreate(SIZE);
-    gplot = gplotCreate("/tmp/rankroot", GPLOT_X11, "sec/MPix vs filter size",
-                        "size", "time");
+    gplot = gplotCreate("/tmp/lept/rank/plots", GPLOT_PNG,
+                        "sec/MPix vs filter size", "size", "time");
+    pixa = pixaCreate(20);
     for (i = 1; i <= SIZE; i++) {
         t1 = t2 = 0.0;
         for (j = 0; j < 5; j++) {
             startTimer();
-            pixt1 = pixRankFilterGray(pixs, i, SIZE + 1, 0.5);
+            pix1 = pixRankFilterGray(pixs, i, SIZE + 1, 0.5);
             t1 += stopTimer();
-            pixDestroy(&pixt1);
+            pixDestroy(&pix1);
             startTimer();
-            pixt1 = pixRankFilterGray(pixs, SIZE + 1, i, 0.5);
+            pix1 = pixRankFilterGray(pixs, SIZE + 1, i, 0.5);
             t2 += stopTimer();
             if (j == 0)
-                pixDisplayWrite(pixt1, 1);
-            pixDestroy(&pixt1);
+                pixaAddPix(pixa, pix1, L_CLONE);
+            pixDestroy(&pix1);
         }
         numaAddNumber(nay1, 1000000. * t1 / (5. * w * h));
         numaAddNumber(nay2, 1000000. * t2 / (5. * w * h));
@@ -127,39 +134,50 @@ static char   mainName[] = "rank_reg";
     gplotAddPlot(gplot, nax, nay2, GPLOT_LINES, "horizontal");
     gplotMakeOutput(gplot);
     gplotDestroy(&gplot);
+    pix1 = pixRead("/tmp/lept/rank/plots.png");
+    pixDisplay(pix1, 100, 100);
+    pixDestroy(&pix1);
 
         /* Display tiled */
-    pixa = pixaReadFiles("/tmp/display", "file");
     pixd = pixaDisplayTiledAndScaled(pixa, 8, 250, 5, 0, 25, 2);
-    pixWrite("/tmp/tiles.jpg", pixd, IFF_JFIF_JPEG);
+    pixDisplay(pixd, 100, 600);
+    pixWrite("/tmp/lept/rank/tiles.jpg", pixd, IFF_JFIF_JPEG);
     pixDestroy(&pixd);
     pixaDestroy(&pixa);
     pixDestroy(&pixs);
 
-    pixDisplayWrite(NULL, -1);  /* clear out */
-
+    /* ------------------     Gray tests    ------------------ */
     pixs = pixRead("test8.jpg");
+    pixa = pixaCreate(4);
     for (i = 1; i <= 4; i++) {
-        pixt1 = pixScaleGrayRank2(pixs, i);
-        pixDisplay(pixt1, 300 * (i - 1), 100);
-        pixDestroy(&pixt1);
+        pix1 = pixScaleGrayRank2(pixs, i);
+        pixaAddPix(pixa, pix1, L_INSERT);
     }
+    pixd = pixaDisplayTiledInRows(pixa, 8, 1500, 1.0, 0, 20, 2);
+    pixDisplay(pixd, 100, 100);
+    pixWrite("/tmp/lept/rank/grayrank2.jpg", pixd, IFF_JFIF_JPEG);
     pixDestroy(&pixs);
+    pixDestroy(&pixd);
+    pixaDestroy(&pixa);
 
     pixs = pixRead("test24.jpg");
-    pixt1 = pixConvertRGBToLuminance(pixs);
-    pixt2 = pixScale(pixt1, 1.5, 1.5);
+    pix1 = pixConvertRGBToLuminance(pixs);
+    pix2 = pixScale(pix1, 1.5, 1.5);
+    pixa = pixaCreate(5);
     for (i = 1; i <= 4; i++) {
         for (j = 1; j <= 4; j++) {
-            pixt3 = pixScaleGrayRankCascade(pixt2, i, j, 0, 0);
-            pixDisplayWrite(pixt3, 1);
-            pixDestroy(&pixt3);
+            pix3 = pixScaleGrayRankCascade(pix2, i, j, 0, 0);
+            pixaAddPix(pixa, pix3, L_INSERT);
         }
     }
-    pixDestroy(&pixt1);
-    pixDestroy(&pixt2);
+    pixd = pixaDisplayTiledInRows(pixa, 8, 1500, 0.7, 0, 20, 2);
+    pixDisplay(pixd, 100, 700);
+    pixWrite("/tmp/lept/rank/graycascade.jpg", pixd, IFF_JFIF_JPEG);
     pixDestroy(&pixs);
-    pixDisplayMultiple("/tmp/display/file*");
+    pixDestroy(&pix1);
+    pixDestroy(&pix2);
+    pixDestroy(&pixd);
+    pixaDestroy(&pixa);
     return 0;
 }
 
