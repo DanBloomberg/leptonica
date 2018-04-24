DanBloomberg/leptonica

commit 374e71e02ee204882921761bba5d2d85dfcb0b4e

Additional fixes to prog tests * Mostly replacing sprintf() by snprintf().

prog/ioformats_reg.c
@@ -341,7 +341,7 @@ L_REGPARAMS  *rp;
             continue;
         }
         d = pixGetDepth(pix);
-        sprintf(psname, "/tmp/lept/regout/junkps.%d", d);
+        snprintf(psname, sizeof(psname), "/tmp/lept/regout/junkps.%d", d);
         fprintf(stderr, "%d bpp\n", d);
         if (test_writemem(pix, IFF_PNM, NULL)) success = FALSE;
         if (test_writemem(pix, IFF_PS, psname)) success = FALSE;
@@ -784,19 +784,19 @@ get_tiff_compression_name(char    *buf,
                           l_int32  format)
 {
     if (format == IFF_TIFF_G4)
-        sprintf(buf, "tiff_g4");
+        snprintf(buf, sizeof(buf), "tiff_g4");
     else if (format == IFF_TIFF_G3)
-        sprintf(buf, "tiff_g3");
+        snprintf(buf, sizeof(buf), "tiff_g3");
     else if (format == IFF_TIFF_ZIP)
-        sprintf(buf, "tiff_zip");
+        snprintf(buf, sizeof(buf), "tiff_zip");
     else if (format == IFF_TIFF_LZW)
-        sprintf(buf, "tiff_lzw");
+        snprintf(buf, sizeof(buf), "tiff_lzw");
     else if (format == IFF_TIFF_RLE)
-        sprintf(buf, "tiff_rle");
+        snprintf(buf, sizeof(buf), "tiff_rle");
     else if (format == IFF_TIFF_PACKBITS)
-        sprintf(buf, "tiff_packbits");
+        snprintf(buf, sizeof(buf), "tiff_packbits");
     else if (format == IFF_TIFF)
-        sprintf(buf, "tiff_uncompressed");
+        snprintf(buf, sizeof(buf), "tiff_uncompressed");
     else
         fprintf(stderr, "format %d: not tiff\n", format);
     return;

prog/barcodetest.c
@@ -70,8 +70,8 @@ static char  mainName[] = "barcodetest";
     sarrayDestroy(&sad2);
 
 /*    sad3 = pixProcessBarcodes(pixs, L_BF_ANY, L_USE_WINDOW, &saw3, 1);
-    sarrayWrite("/tmp/saw3", saw3);
-    sarrayWrite("/tmp/sad3", sad3);
+    sarrayWrite("/tmp/lept/barc/saw3", saw3);
+    sarrayWrite("/tmp/lept/barc/sad3", sad3);
     sarrayDestroy(&saw3);
     sarrayDestroy(&sad3); */
 

prog/binmorph1_reg.c
@@ -130,13 +130,13 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix2 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "d%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "d%d.%d", WIDTH, HEIGHT);
     pix3 = pixMorphSequence(pixs, sequence, 0);    /* sequence, atomic */
     pixEqual(pixref, pix3, &same);
     if (!same) {
         fprintf(stderr, "pixref != pix3 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "d%d.1 + d1.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "d%d.1 + d1.%d", WIDTH, HEIGHT);
     pix4 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable */
     pixEqual(pixref, pix4, &same);
     if (!same) {
@@ -182,7 +182,7 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix11 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "d%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "d%d.%d", WIDTH, HEIGHT);
     pix12 = pixMorphCompSequence(pixs, sequence, 0);    /* comp sequence */
     pixEqual(pixref, pix12, &same);
     if (!same) {
@@ -223,13 +223,13 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix2 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "e%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "e%d.%d", WIDTH, HEIGHT);
     pix3 = pixMorphSequence(pixs, sequence, 0);    /* sequence, atomic */
     pixEqual(pixref, pix3, &same);
     if (!same) {
         fprintf(stderr, "pixref != pix3 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "e%d.1 + e1.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "e%d.1 + e1.%d", WIDTH, HEIGHT);
     pix4 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable */
     pixEqual(pixref, pix4, &same);
     if (!same) {
@@ -276,7 +276,7 @@ SEL     *sel;
         fprintf(stderr, "pixref != pix11 !\n"); ok = FALSE;
     }
 
-    sprintf(sequence, "e%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "e%d.%d", WIDTH, HEIGHT);
     pix12 = pixMorphCompSequence(pixs, sequence, 0);    /* comp sequence */
     pixEqual(pixref, pix12, &same);
     if (!same) {
@@ -317,20 +317,21 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix2 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "o%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "o%d.%d", WIDTH, HEIGHT);
     pix3 = pixMorphSequence(pixs, sequence, 0);    /* sequence, atomic */
     pixEqual(pixref, pix3, &same);
     if (!same) {
         fprintf(stderr, "pixref != pix3 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "e%d.%d + d%d.%d", WIDTH, HEIGHT, WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "e%d.%d + d%d.%d",
+             WIDTH, HEIGHT, WIDTH, HEIGHT);
     pix4 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable */
     pixEqual(pixref, pix4, &same);
     if (!same) {
         fprintf(stderr, "pixref != pix4 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "e%d.1 + e1.%d + d%d.1 + d1.%d", WIDTH, HEIGHT,
-            WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "e%d.1 + e1.%d + d%d.1 + d1.%d",
+             WIDTH, HEIGHT, WIDTH, HEIGHT);
     pix5 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable^2 */
     pixEqual(pixref, pix5, &same);
     if (!same) {
@@ -370,7 +371,7 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix11 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "o%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "o%d.%d", WIDTH, HEIGHT);
     pix12 = pixMorphCompSequence(pixs, sequence, 0);    /* comp sequence */
     pixEqual(pixref, pix12, &same);
     if (!same) {
@@ -420,14 +421,15 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix2 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "d%d.%d + e%d.%d", WIDTH, HEIGHT, WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "d%d.%d + e%d.%d",
+             WIDTH, HEIGHT, WIDTH, HEIGHT);
     pix3 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable */
     pixEqual(pixref, pix3, &same);
     if (!same) {
         fprintf(stderr, "pixref != pix3 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "d%d.1 + d1.%d + e%d.1 + e1.%d", WIDTH, HEIGHT,
-            WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "d%d.1 + d1.%d + e%d.1 + e1.%d",
+             WIDTH, HEIGHT, WIDTH, HEIGHT);
     pix4 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable^2 */
     pixEqual(pixref, pix4, &same);
     if (!same) {
@@ -474,20 +476,21 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix2 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "c%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "c%d.%d", WIDTH, HEIGHT);
     pix3 = pixMorphSequence(pixs, sequence, 0);    /* sequence, atomic */
     pixEqual(pixref, pix3, &same);
     if (!same) {
         fprintf(stderr, "pixref != pix3 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "b32 + d%d.%d + e%d.%d", WIDTH, HEIGHT, WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "b32 + d%d.%d + e%d.%d",
+             WIDTH, HEIGHT, WIDTH, HEIGHT);
     pix4 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable */
     pixEqual(pixref, pix4, &same);
     if (!same) {
         fprintf(stderr, "pixref != pix4 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "b32 + d%d.1 + d1.%d + e%d.1 + e1.%d", WIDTH, HEIGHT,
-            WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "b32 + d%d.1 + d1.%d + e%d.1 + e1.%d",
+             WIDTH, HEIGHT, WIDTH, HEIGHT);
     pix5 = pixMorphSequence(pixs, sequence, 0);    /* sequence, separable^2 */
     pixEqual(pixref, pix5, &same);
     if (!same) {
@@ -527,7 +530,7 @@ SEL     *sel;
     if (!same) {
         fprintf(stderr, "pixref != pix11 !\n"); ok = FALSE;
     }
-    sprintf(sequence, "c%d.%d", WIDTH, HEIGHT);
+    snprintf(sequence, sizeof(sequence), "c%d.%d", WIDTH, HEIGHT);
     pix12 = pixMorphCompSequence(pixs, sequence, 0);    /* comp sequence */
     pixEqual(pixref, pix12, &same);
     if (!same) {

prog/binmorph2_reg.c
@@ -64,8 +64,8 @@ static char  mainName[] = "binmorph2_reg";
         fprintf(stderr, "%d: (%d, %d): %d\n", i, factor1, factor2, diff);
 
             /* Carry out operations on identical sized Sels: dilation */
-        sprintf(buffer1, "d%d.%d", i + diff, i + diff);
-        sprintf(buffer2, "d%d.%d", i, i);
+        snprintf(buffer1, sizeof(buffer1), "d%d.%d", i + diff, i + diff);
+        snprintf(buffer2, sizeof(buffer2), "d%d.%d", i, i);
         pixt1 = pixMorphSequence(pixsd, buffer1, 0);
         pixt2 = pixMorphCompSequence(pixsd, buffer2, 0);
         pixEqual(pixt1, pixt2, &same);
@@ -87,8 +87,8 @@ static char  mainName[] = "binmorph2_reg";
         pixDestroy(&pixt3);
 
             /* ... erosion */
-        sprintf(buffer1, "e%d.%d", i + diff, i + diff);
-        sprintf(buffer2, "e%d.%d", i, i);
+        snprintf(buffer1, sizeof(buffer1), "e%d.%d", i + diff, i + diff);
+        snprintf(buffer2, sizeof(buffer2), "e%d.%d", i, i);
         pixt1 = pixMorphSequence(pixsd, buffer1, 0);
         pixt2 = pixMorphCompSequence(pixsd, buffer2, 0);
         pixEqual(pixt1, pixt2, &same);
@@ -110,8 +110,8 @@ static char  mainName[] = "binmorph2_reg";
         pixDestroy(&pixt3);
 
             /* ... opening */
-        sprintf(buffer1, "o%d.%d", i + diff, i + diff);
-        sprintf(buffer2, "o%d.%d", i, i);
+        snprintf(buffer1, sizeof(buffer1), "o%d.%d", i + diff, i + diff);
+        snprintf(buffer2, sizeof(buffer2), "o%d.%d", i, i);
         pixt1 = pixMorphSequence(pixsd, buffer1, 0);
         pixt2 = pixMorphCompSequence(pixsd, buffer2, 0);
         pixEqual(pixt1, pixt2, &same);
@@ -133,8 +133,8 @@ static char  mainName[] = "binmorph2_reg";
         pixDestroy(&pixt3);
 
             /* ... closing */
-        sprintf(buffer1, "c%d.%d", i + diff, i + diff);
-        sprintf(buffer2, "c%d.%d", i, i);
+        snprintf(buffer1, sizeof(buffer1), "c%d.%d", i + diff, i + diff);
+        snprintf(buffer2, sizeof(buffer2), "c%d.%d", i, i);
         pixt1 = pixMorphSequence(pixsd, buffer1, 0);
         pixt2 = pixMorphCompSequence(pixsd, buffer2, 0);
         pixEqual(pixt1, pixt2, &same);

prog/colormorph_reg.c
@@ -50,7 +50,7 @@ L_REGPARAMS  *rp;
 
     pix1 = pixColorMorph(pixs, L_MORPH_DILATE, SIZE, SIZE);
     regTestWritePixAndCheck(rp, pix1, IFF_JFIF_JPEG);  /* 0 */
-    sprintf(buf, "d%d.%d", SIZE, SIZE);
+    snprintf(buf, sizeof(buf), "d%d.%d", SIZE, SIZE);
     pix2 = pixColorMorphSequence(pixs, buf, 0, 0);
     regTestComparePix(rp, pix1, pix2);  /* 1 */
     pixaAddPix(pixa, pix1, L_INSERT);
@@ -58,7 +58,7 @@ L_REGPARAMS  *rp;
 
     pix1 = pixColorMorph(pixs, L_MORPH_ERODE, SIZE, SIZE);
     regTestWritePixAndCheck(rp, pix1, IFF_JFIF_JPEG);  /* 2 */
-    sprintf(buf, "e%d.%d", SIZE, SIZE);
+    snprintf(buf, sizeof(buf), "e%d.%d", SIZE, SIZE);
     pix2 = pixColorMorphSequence(pixs, buf, 0, 0);
     regTestComparePix(rp, pix1, pix2);  /* 3 */
     pixaAddPix(pixa, pix1, L_INSERT);
@@ -66,7 +66,7 @@ L_REGPARAMS  *rp;
 
     pix1 = pixColorMorph(pixs, L_MORPH_OPEN, SIZE, SIZE);
     regTestWritePixAndCheck(rp, pix1, IFF_JFIF_JPEG);  /* 4 */
-    sprintf(buf, "o%d.%d", SIZE, SIZE);
+    snprintf(buf, sizeof(buf), "o%d.%d", SIZE, SIZE);
     pix2 = pixColorMorphSequence(pixs, buf, 0, 0);
     regTestComparePix(rp, pix1, pix2);  /* 5 */
     pixaAddPix(pixa, pix1, L_INSERT);
@@ -74,7 +74,7 @@ L_REGPARAMS  *rp;
 
     pix1 = pixColorMorph(pixs, L_MORPH_CLOSE, SIZE, SIZE);
     regTestWritePixAndCheck(rp, pix1, IFF_JFIF_JPEG);  /* 6 */
-    sprintf(buf, "c%d.%d", SIZE, SIZE);
+    snprintf(buf, sizeof(buf), "c%d.%d", SIZE, SIZE);
     pix2 = pixColorMorphSequence(pixs, buf, 0, 0);
     regTestComparePix(rp, pix1, pix2);  /* 7 */
     pixaAddPix(pixa, pix1, L_INSERT);

prog/colorquant_reg.c
@@ -219,7 +219,7 @@ PIXA      *pixa;
 
     pixd = pixaDisplay(pixa, 0, 0);
     pixDisplayWithTitle(pixd, 100, 100, NULL, rp->display);
-    sprintf(buf, "/tmp/lept/regout/disp.%d.jpg", i);
+    snprintf(buf, sizeof(buf), "/tmp/lept/regout/disp.%d.jpg", i);
     pixWrite(buf, pixd, IFF_JFIF_JPEG);
 
     pixDestroy(&pixs);

prog/colorspace_reg.c
@@ -161,7 +161,7 @@ L_REGPARAMS  *rp;
                          "white point space for red", "amount of color");
     for (j = 0; j < 6; j++) {
         na = numaaGetNuma(naa1, j, L_CLONE);
-        sprintf(label, "thresh %d", 30 + 10 * j);
+        snprintf(label, sizeof(label), "thresh %d", 30 + 10 * j);
         gplotAddPlot(gplot1, naseq, na, GPLOT_LINES, label);
         numaDestroy(&na);
         na = numaaGetNuma(naa2, j, L_CLONE);

prog/concatpdf.c
@@ -40,7 +40,7 @@
  *    Syntax: concatpdf dir [pattern]
  *        where pattern is an optional string to be matched
  *
- *    The output goes to:  /tmp/lept/output.pdf
+ *    The output goes to:  /tmp/lept/image/output.pdf
  *
  *    This works by converting to PostScript (without annotations),
  *    then rasterizing the images, and finally generating a pdf from
@@ -56,7 +56,7 @@
  *    (2) Use ps2pdf-gray from Ghostscript to rasterize the images.
  *        The images are written to /tmp/lept/image/
  *    (3) Use convertFilesToPdf to generate a pdf file,
- *        /tmp/lept/iamge/output.pdf, from the images.
+ *        /tmp/lept/image/output.pdf, from the images.
  */
 
 #include <sys/stat.h>

commit 70cbcbad238b8d743c19456d7734e71ecc6f151f

ioformats_reg: Fix wrong snprintf and uninitialized variable
Commit 374e71e02ee204882921761bba5d2d85dfcb0b4e introduced a bug which
is reported by gcc:

prog/ioformats_reg.c:787:29: warning:
 argument to ‘sizeof’ in ‘snprintf’ call is the same expression as the
 destination; did you mean to provide an explicit length? [-Wsizeof-pointer-memaccess]

Fix also a typo in the 2nd fprintf statement and make sure that there is
always a valid format name:

prog/ioformats_reg.c:774:13: warning:
 ‘tiff_compression_name’ may be used uninitialized in this function [-Wmaybe-uninitialized]

Signed-off-by: Stefan Weil <sw@weilnetz.de>

prog/ioformats_reg.c
@@ -87,7 +87,7 @@ static l_int32 testcomp_mem(PIX *pixs, PIX **ppixt, l_int32 index,
 static l_int32 test_writemem(PIX *pixs, l_int32 format, char *psfile);
 static PIX *make_24_bpp_pix(PIX *pixs);
 static l_int32 get_header_data(const char *filename, l_int32 true_format);
-static void get_tiff_compression_name(char *buf, l_int32 format);
+static const char *get_tiff_compression_name(l_int32 format);
 
 LEPT_DLL extern const char *ImageFileFormatExtensions[];
 
@@ -705,7 +705,7 @@ static l_int32
 get_header_data(const char  *filename,
                 l_int32      true_format)
 {
-char      buf[64];
+const char *tiff_compression_name = "undefined";
 l_uint8  *data;
 l_int32   ret1, ret2, format1, format2;
 l_int32   w1, w2, h1, h2, d1, d2, bps1, bps2, spp1, spp2, iscmap1, iscmap2;
@@ -737,11 +737,12 @@ size_t    size1, size2;
         fprintf(stderr, "Error: couldn't read header data: %s\n", filename);
     else {
         if (format1 > IFF_PNG && format1 < IFF_PNM) {
-            get_tiff_compression_name(buf, format1);
+            tiff_compression_name = get_tiff_compression_name(format1);
             fprintf(stderr, "Format data for image %s with format %s:\n"
                 "  nbytes = %lu, size (w, h, d) = (%d, %d, %d)\n"
                 "  bps = %d, spp = %d, iscmap = %d\n",
-                filename, buf, (unsigned long)size1, w1, h1, d1,
+                filename, tiff_compression_name,
+                (unsigned long)size1, w1, h1, d1,
                 bps1, spp1, iscmap1);
         } else {
             fprintf(stderr, "Format data for image %s with format %s:\n"
@@ -770,8 +771,8 @@ size_t    size1, size2;
         if (size1 != size2 || format1 != format2 || w1 != w2 ||
             h1 != h2 || d1 != d2 || bps1 != bps2 || spp1 != spp2 ||
             iscmap1 != iscmap2) {
-            fprintf(stderr, "Incomsistency reading image %s with format %s\n",
-                    filename, buf);
+            fprintf(stderr, "Inconsistency reading image %s with format %s\n",
+                    filename, tiff_compression_name);
             ret2 = 1;
         }
     }
@@ -779,25 +780,25 @@ size_t    size1, size2;
 }
 
 
-static void
-get_tiff_compression_name(char    *buf,
-                          l_int32  format)
+static const char *
+get_tiff_compression_name(l_int32  format)
 {
+    const char *tiff_compression_name = "unknown";
     if (format == IFF_TIFF_G4)
-        snprintf(buf, sizeof(buf), "tiff_g4");
+        tiff_compression_name = "tiff_g4";
     else if (format == IFF_TIFF_G3)
-        snprintf(buf, sizeof(buf), "tiff_g3");
+        tiff_compression_name =  "tiff_g3";
     else if (format == IFF_TIFF_ZIP)
-        snprintf(buf, sizeof(buf), "tiff_zip");
+        tiff_compression_name =  "tiff_zip";
     else if (format == IFF_TIFF_LZW)
-        snprintf(buf, sizeof(buf), "tiff_lzw");
+        tiff_compression_name =  "tiff_lzw";
     else if (format == IFF_TIFF_RLE)
-        snprintf(buf, sizeof(buf), "tiff_rle");
+        tiff_compression_name =  "tiff_rle";
     else if (format == IFF_TIFF_PACKBITS)
-        snprintf(buf, sizeof(buf), "tiff_packbits");
+        tiff_compression_name =  "tiff_packbits";
     else if (format == IFF_TIFF)
-        snprintf(buf, sizeof(buf), "tiff_uncompressed");
+        tiff_compression_name = "tiff_uncompressed";
     else
         fprintf(stderr, "format %d: not tiff\n", format);
-    return;
+    return tiff_compression_name;
 }