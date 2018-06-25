7fed79dbe0240fc18d41ce1e5f98531679b91ae2
Dan Bloomberg <dan.bloomberg@gmail.com>
2018-06-25 14:36:27 -0700

    Fixed an OOB read from a buffer in pngio.c (potential security issue) * This was in pixReadMemPng()

--- src/pngio.c
+++ src/pngio.c
@@ -1461,6 +1461,9 @@ memio_png_read_data(png_structp  png_ptr,
 MEMIODATA  *thing;
 
     thing = (MEMIODATA *)png_get_io_ptr(png_ptr);
+    if (byteCountToRead > (thing->m_Size - thing->m_Count)) {
+      png_error(png_ptr, "read error in memio_png_read_data");
+    }
     memcpy(outBytes, thing->m_Buffer + thing->m_Count, byteCountToRead);
     thing->m_Count += byteCountToRead;
 }
@@ -2101,4 +2104,3 @@ MEMIODATA    state;
 /* --------------------------------------------*/
 #endif  /* HAVE_LIBPNG */
 /* --------------------------------------------*/
-
