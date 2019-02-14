e83233c2d878713d07b38fab7fc253497fc44ac3
Egor Pugin <egor.pugin@gmail.com>
2019-02-15 00:38:19 +0300

    Update sw script.

--- sw.cpp
+++ sw.cpp
@@ -33,7 +33,7 @@ void build(Solution &s)
         leptonica += "org.sw.demo.tiff-4"_dep;
         leptonica += "org.sw.demo.webmproject.webp-*"_dep;
 
-        if (leptonica.Variables["WORDS_BIGENDIAN"] == "1")
+        if (leptonica.Variables["WORDS_BIGENDIAN"] == 1)
             leptonica.Variables["ENDIANNESS"] = "L_BIG_ENDIAN";
         else
             leptonica.Variables["ENDIANNESS"] = "L_LITTLE_ENDIAN";
