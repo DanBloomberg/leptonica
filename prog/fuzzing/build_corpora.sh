02a10392eb5084da52cd586093a3931dff4c5cfa
Adam Korczynski <adam@adalogics.com>
2020-04-29 20:51:18 +0100

    Added script to build corpora of fuzzers

---
+++ prog/fuzzing/build_corpora.sh
@@ -0,0 +1,4 @@
+cp $SRC/leptonica/prog/fuzzing/general_corpus.zip $OUT/pix_rotate_shear_fuzzer_seed_corpus.zip
+cp $SRC/leptonica/prog/fuzzing/general_corpus.zip $OUT/enhance_fuzzer_seed_corpus.zip
+
+cp $SRC/leptonica/prog/fuzzing/pixa_recog_fuzzer_seed_corpus.zip $OUT/
