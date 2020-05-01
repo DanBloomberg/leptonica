25e0b196d0d2fcbf6c71c0b85637721f80d190c3
Adam Korczynski <adam@adalogics.com>
2020-05-01 18:14:14 +0100

    Changed pixReadMem to pixReadMemSpix

--- prog/fuzzing/colorquant_fuzzer.cc
+++ prog/fuzzing/colorquant_fuzzer.cc
@@ -5,7 +5,7 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     PIX *pixs, *pix1;
 
-    pixs = pixReadMem(data, size);
+    pixs = pixReadMemSpix(data, size);
     if(pixs==NULL) return 0;
     
     pix1 = pixFewColorsMedianCutQuantMixed(pixs, 30, 30, 100, 0, 0, 0);
