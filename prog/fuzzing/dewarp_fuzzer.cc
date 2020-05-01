edd0c947c6cd6682cc1a0477ee697d123ef9515b
Adam Korczynski <adam@adalogics.com>
2020-05-01 18:28:11 +0100

    Added new fuzzer

---
+++ prog/fuzzing/dewarp_fuzzer.cc
@@ -0,0 +1,26 @@
+#include "allheaders.h"
+
+extern "C" int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    if(size<3) return 0;
+
+    PIX *pixs, *pixd;
+    L_DEWARPA *dewa1;
+    PIXAC *pixac;
+    SARRAY *sa;
+
+    pixs = pixReadMemSpix(data, size);
+    if(pixs==NULL) return 0;
+    
+    dewarpSinglePage(pixs, 0, 1, 1, 0, &pixd, NULL, 1);
+	
+    pixac = pixacompReadMem(data, size);
+    dewa1 = dewarpaCreateFromPixacomp(pixac, 1, 0, 10, -1);
+    
+    dewarpaDestroy(&dewa1);
+    pixacompDestroy(&pixac);
+    pixDestroy(&pixs);
+    pixDestroy(&pixd);
+    return 0;
+}
