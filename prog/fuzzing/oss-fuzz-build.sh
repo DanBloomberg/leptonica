b7a0e3dc63d97da68fe22ecdb6b78bb5e2fca2d0
Adam Korczynski <adam@adalogics.com>
2020-05-05 21:21:21 +0100

    Added seed corpus for barcode fuzzer

--- prog/fuzzing/oss-fuzz-build.sh
+++ prog/fuzzing/oss-fuzz-build.sh
@@ -108,3 +108,4 @@ cp $SRC/leptonica/prog/fuzzing/general_corpus.zip $OUT/pix_rotate_shear_fuzzer_s
 cp $SRC/leptonica/prog/fuzzing/general_corpus.zip $OUT/enhance_fuzzer_seed_corpus.zip
 
 cp $SRC/leptonica/prog/fuzzing/pixa_recog_fuzzer_seed_corpus.zip $OUT/
+cp $SRC/leptonica/prog/fuzzing/barcode_fuzzer_seed_corpus.zip $OUT/
