87c6a0d4ea6762fdb852aed4651eddcff12cb0c5
Adam Korczynski <adam@adalogics.com>
2020-05-13 21:44:43 +0100

    Cleaned up variable names

--- prog/fuzzing/pageseg_fuzzer.cc
+++ prog/fuzzing/pageseg_fuzzer.cc
@@ -7,8 +7,8 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 
     l_int32      score;
     PIX          *pixs;
-    PIX          *pixhm, *pixtm, *pixtb, *ppixdebug;
-    PIXA         *pixadb, *ppixa;
+    PIX          *pix1, *pix2, *pix3, *pix4;
+    PIXA         *pixa1, *pixa2;
     PIXAC        *pixac;
     BOX          *box;
     BOXA         *boxa;
@@ -16,26 +16,26 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     pixs = pixReadMemSpix(data, size);
     if(pixs==NULL) return 0;
 
-    pixadb = pixaCreate(0);
-    pixDecideIfTable(pixs, NULL, L_PORTRAIT_MODE, &score, pixadb);
-    pixaDestroy(&pixadb);
+    pixa1 = pixaCreate(0);
+    pixDecideIfTable(pixs, NULL, L_PORTRAIT_MODE, &score, pixa1);
+    pixaDestroy(&pixa1);
 
-    pixadb = pixaCreate(0);
-    pixGetRegionsBinary(pixs, &pixhm, &pixtm, &pixtb, pixadb);
-    pixDestroy(&pixhm);
-    pixDestroy(&pixtm);
-    pixDestroy(&pixtb);
-    pixaDestroy(&pixadb);
+    pixa1 = pixaCreate(0);
+    pixGetRegionsBinary(pixs, &pix1, &pix2, &pix3, pixa1);
+    pixDestroy(&pix1);
+    pixDestroy(&pix2);
+    pixDestroy(&pix3);
+    pixaDestroy(&pixa1);
 
     pixac = pixacompReadMem(data, size);
     box = pixFindPageForeground(pixs, 170, 70, 30, 0, pixac);
     boxDestroy(&box);
     pixacompDestroy(&pixac);
 
-    pixSplitIntoCharacters(pixs, 4, 4, &boxa, &ppixa, &ppixdebug);
+    pixSplitIntoCharacters(pixs, 4, 4, &boxa, &pixa2, &pix4);
     boxaDestroy(&boxa);
-    pixaDestroy(&ppixa);
-    pixDestroy(&ppixdebug);
+    pixaDestroy(&pixa2);
+    pixDestroy(&pix4);
 
 
     pixDestroy(&pixs);
