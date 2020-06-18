3fbf3b2127539488ec796522098946d07da28c30
Dan Bloomberg <dan.bloomberg@gmail.com>
2020-06-18 16:03:32 -0700

    Refactor rasteroptest to allow heavy testing on specific ops

--- prog/rasteroptest.c
+++ prog/rasteroptest.c
@@ -46,14 +46,15 @@
 
 #include "allheaders.h"
 
-static const l_int32  niters = 1000;
+void GeneralTest(PIX *pix1, BOX *box1, BOX *box2, l_int32 op, l_int32 niters);
+void InplaceTest(PIX *pix1, BOX *box1, BOX *box2, l_int32 op, l_int32 niters);
 
 int main(int    argc,
          char **argv)
 {
 BOX         *box1, *box2;
-PIX         *pix1, *pix2, *pix3;
-l_int32      op, i, val1, val2, val3, val4, val5, val6;
+PIX         *pix1;
+l_int32      niters, op, selectop;
 static char  mainName[] = "rasteroptest";
 
     setLeptDebugOK(1);
@@ -61,54 +62,24 @@ static char  mainName[] = "rasteroptest";
     pix1 = pixRead("test24.jpg");
     box1 = boxCreate(243, 127, 513, 359);
     box2 = boxCreate(541, 312, 513, 359);
+    niters = 10000;
+    selectop = PIX_SRC;
 
 #if 1
         /* Basic rasterop */
-    for (op = 0; op < 16; op++) {
-        startTimer();
-        for (i = 0; i < niters; i++) {
-            pix2 = pixClipRectangle(pix1, box1, NULL);
-            pix3 = pixClipRectangle(pix1, box2, NULL);
-            genRandomIntOnInterval(-42, 403, 0, &val1);
-            genRandomIntOnInterval(-18, 289, 0, &val2);
-            genRandomIntOnInterval(13, 289, 0, &val3);
-            genRandomIntOnInterval(13, 403, 0, &val4);
-            genRandomIntOnInterval(-34, 289, 0, &val5);
-            genRandomIntOnInterval(-38, 403, 0, &val6);
-            pixRasterop(pix3, val1, val2, val3, val4, op, pix2, val5, val6);
-            if (i == op) {
-                lept_stderr("Rasterop: op = %d    ", op);
-                pixDisplay(pix3, 100 * i, 100);
-            }
-            pixDestroy(&pix2);
-            pixDestroy(&pix3);
-        }
-        lept_stderr("Time = %7.3f sec\n", stopTimer());
-    }
+    for (op = 0; op < 16; op++)
+        GeneralTest(pix1, box1, box2, op, niters);
 #endif
 
 #if 1
         /* In-place rasterop */
-    for (op = 0; op < 16; op++) {
-        startTimer();
-        for (i = 0; i < niters; i++) {
-            pix2 = pixClipRectangle(pix1, box1, NULL);
-            genRandomIntOnInterval(-217, 113, 0, &val1);
-            genRandomIntOnInterval(1, 211, 0, &val2);
-            genRandomIntOnInterval(-217, 143, 0, &val3);
-            genRandomIntOnInterval(-247, 113, 0, &val4);
-            genRandomIntOnInterval(1, 241, 0, &val5);
-            genRandomIntOnInterval(-113, 163, 0, &val6);
-            pixRasteropHip(pix2, val1, val2, val3, L_BRING_IN_WHITE);
-            pixRasteropVip(pix2, val4, val5, val6, L_BRING_IN_BLACK);
-            if (i == op) {
-                lept_stderr("Rasterop: op = %d    ", op);
-                pixDisplay(pix2, 100 * i, 500);
-            }
-            pixDestroy(&pix2);
-        }
-        lept_stderr("Time = %7.3f sec\n", stopTimer());
-    }
+    for (op = 0; op < 16; op++)
+        InplaceTest(pix1, box1, box2, op, niters);
+#endif
+
+#if 0
+        /* Basic rasterop; single operation */
+    GeneralTest(pix1, box1, box2, selectop, niters);
 #endif
 
     pixDestroy(&pix1);
@@ -117,3 +88,55 @@ static char  mainName[] = "rasteroptest";
     return 0;
 }
 
+/* ------------------------------------------------------------------- */
+void GeneralTest(PIX *pix1, BOX *box1, BOX *box2, l_int32 op, l_int32 niters)
+{
+PIX     *pix2, *pix3;
+l_int32  i, val1, val2, val3, val4, val5, val6;
+
+    startTimer();
+    for (i = 0; i < niters; i++) {
+        pix2 = pixClipRectangle(pix1, box1, NULL);
+        pix3 = pixClipRectangle(pix1, box2, NULL);
+        genRandomIntOnInterval(-42, 403, 0, &val1);
+        genRandomIntOnInterval(-18, 289, 0, &val2);
+        genRandomIntOnInterval(13, 289, 0, &val3);
+        genRandomIntOnInterval(13, 403, 0, &val4);
+        genRandomIntOnInterval(-34, 289, 0, &val5);
+        genRandomIntOnInterval(-38, 403, 0, &val6);
+        pixRasterop(pix3, val1, val2, val3, val4, op, pix2, val5, val6);
+        if (i == op) {
+            lept_stderr("Rasterop: op = %d    ", op);
+            pixDisplay(pix3, 100 * i, 100);
+        }
+        pixDestroy(&pix2);
+        pixDestroy(&pix3);
+    }
+    lept_stderr("Time = %7.3f sec\n", stopTimer());
+}
+
+/* ------------------------------------------------------------------- */
+void InplaceTest(PIX *pix1, BOX *box1, BOX *box2, l_int32 op, l_int32 niters)
+{
+PIX     *pix2;
+l_int32  i, val1, val2, val3, val4, val5, val6;
+
+    startTimer();
+    for (i = 0; i < niters; i++) {
+        pix2 = pixClipRectangle(pix1, box1, NULL);
+        genRandomIntOnInterval(-217, 113, 0, &val1);
+        genRandomIntOnInterval(1, 211, 0, &val2);
+        genRandomIntOnInterval(-217, 143, 0, &val3);
+        genRandomIntOnInterval(-247, 113, 0, &val4);
+        genRandomIntOnInterval(1, 241, 0, &val5);
+        genRandomIntOnInterval(-113, 163, 0, &val6);
+        pixRasteropHip(pix2, val1, val2, val3, L_BRING_IN_WHITE);
+        pixRasteropVip(pix2, val4, val5, val6, L_BRING_IN_BLACK);
+        if (i == op) {
+            lept_stderr("Rasterop: op = %d    ", op);
+            pixDisplay(pix2, 100 * i, 500);
+        }
+        pixDestroy(&pix2);
+    }
+    lept_stderr("Time = %7.3f sec\n", stopTimer());
+}
