c3c8a2130d8309b420d39c8b79c66182e6455e77
Dan Bloomberg <dan.bloomberg@gmail.com>
2020-08-19 17:48:56 -0700

    Increment release number at head to 1.81.0 * No patches have been suggested for 1.80.0 in 3 weeks since release.

--- sw.cpp
+++ sw.cpp
@@ -19,7 +19,7 @@ void build(Solution &s)
         t += "org.sw.demo.webmproject.webp"_dep;
     };
 
-    auto &leptonica = s.addTarget<LibraryTarget>("danbloomberg.leptonica", "1.80.0");
+    auto &leptonica = s.addTarget<LibraryTarget>("danbloomberg.leptonica", "1.81.0");
     leptonica += Git("https://github.com/DanBloomberg/leptonica");
 
     {
