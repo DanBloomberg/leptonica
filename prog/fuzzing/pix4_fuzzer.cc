700cb86fd59a2cfe32a0657f95adbc8ffbf2138d
Dan Bloomberg <dan.bloomberg@gmail.com>
2020-08-21 21:42:30 -0700

    Issue 25121: pix4_fuzzer: uninit value in pixGetCmapHistogramInRect * Trivially fixed; the fuzzer used a garbage pix input.

--- prog/fuzzing/pix4_fuzzer.cc
+++ prog/fuzzing/pix4_fuzzer.cc
@@ -28,7 +28,7 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 
     pix1 = pixConvertTo8(pixs, FALSE);
     box = boxCreate(120, 30, 200, 200);
-    na1 = pixGetCmapHistogramInRect(pix2, box, 1);
+    na1 = pixGetCmapHistogramInRect(pix1, box, 1);
     numaDestroy(&na1);
     boxDestroy(&box);
     pixDestroy(&pix1);
