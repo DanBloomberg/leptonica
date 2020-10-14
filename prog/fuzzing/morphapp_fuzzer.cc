6c6154acf2d6c385bb7bf702616b2b7cf6e9ed51
Dan Bloomberg <dan.bloomberg@gmail.com>
2020-10-14 15:03:06 -0700

    Added Adam's 10 new fuzz targets to Makefile.am in root * Also renamed recog_basic.cc --> recog_basic_fuzzer.cc so that it would   be built by prog/fuzzing/oss-fuzz-build.sh * Also fixed some bugs in morphapp_fuzzer.cc.

--- prog/fuzzing/morphapp_fuzzer.cc
+++ prog/fuzzing/morphapp_fuzzer.cc
@@ -6,49 +6,45 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
     leptSetStdNullHandler();
 
-
-    PIX *pixs_payload = pixReadMemSpix(data, size);
+    Pix *pixs_payload = pixReadMemSpix(data, size);
     if(pixs_payload == NULL) return 0;
 
-    PIX *pix1;
-    PIX *pix2;
-
-    PIX *pix1 = pixRead("../test8.jpg");
-    PIX *pix2 = pixRead("../test8.jpg");
-    PIX *pix_return1 = pixDisplayMatchedPattern(pixs_payload, pix1, pix2,  1,  2, 3, 0.5, 1);
+    Pix *pix1 = pixRead("../test8.jpg");
+    Pix *pix2 = pixRead("../test8.jpg");
+    Pix *pix_return1 = pixDisplayMatchedPattern(pixs_payload, pix1, pix2,
+                                                1, 2, 3, 0.5, 1);
     pixDestroy(&pix1);
     pixDestroy(&pix_return1);
     pixDestroy(&pix2);
 
-
-    PIX *pix_return2 = pixFastTophat(pixs_payload, 2, 2, L_TOPHAT_WHITE);
+    Pix *pix_return2 = pixFastTophat(pixs_payload, 2, 2, L_TOPHAT_WHITE);
     pixDestroy(&pix_return2);
 
-    PIX *pix_return3 = pixHDome(pixs_payload, 1, 4);
+    Pix *pix_return3 = pixHDome(pixs_payload, 1, 4);
     pixDestroy(&pix_return3);
 
-    SELA *sela1 = selaCreate(0);
-    PIX *pix_return4 = pixIntersectionOfMorphOps(pixs_payload, sela1, L_MORPH_DILATE);
+    Sela *sela1 = selaCreate(0);
+    Pix *pix_return4 = pixIntersectionOfMorphOps(pixs_payload,
+                                                 sela1, L_MORPH_DILATE);
     selaDestroy(&sela1);
     pixDestroy(&pix_return4);
 
-    PIX *pix_return5 = pixMorphGradient(pixs_payload, 5, 5, 1);
+    Pix *pix_return5 = pixMorphGradient(pixs_payload, 5, 5, 1);
     pixDestroy(&pix_return5);
 
-
     pix1 = pixRead("../test8.jpg");
-    BOXA *boxa1;
+    Boxa *boxa1;
     const char *sequence = "sequence";
-    PIX *pix_return6 = pixMorphSequenceByRegion(pixs_payload, pix1, sequence, 4, 1, 1, &boxa1);
+    Pix *pix_return6 = pixMorphSequenceByRegion(pixs_payload, pix1,
+                                                sequence, 4, 1, 1, &boxa1);
     boxaDestroy(&boxa1);
     pixDestroy(&pix1);
     pixDestroy(&pix_return6);
 
     pix1 = pixRead("../test8.jpg");
-    PIX *pix_return7 = pixMorphSequenceMasked(pixs_payload, pix1, sequence, 0);
+    Pix *pix_return7 = pixMorphSequenceMasked(pixs_payload, pix1, sequence, 0);
     pixDestroy(&pix1);
     pixDestroy(&pix_return7);
-
 
     pix1 = pixCreate(300, 300, 32);
     pix2 = pixCreate(300, 300, 32);
@@ -56,14 +52,12 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     pixDestroy(&pix1);
     pixDestroy(&pix2);
 
-
-    pix = pixCreate(300, 300, 32);
-    PIX *pix_return8 = pixSeedfillMorph(pixs_payload, pix, 0, 4);
+    Pix *pix = pixCreate(300, 300, 32);
+    Pix *pix_return8 = pixSeedfillMorph(pixs_payload, pix, 0, 4);
     pixDestroy(&pix_return8);
     pixDestroy(&pix);
 
-
-    PIX *pix_return9 = pixSelectiveConnCompFill(pixs_payload, 4, 1, 1);
+    Pix *pix_return9 = pixSelectiveConnCompFill(pixs_payload, 4, 1, 1);
     pixDestroy(&pix_return9);
 
     pixDestroy(&pixs_payload);
