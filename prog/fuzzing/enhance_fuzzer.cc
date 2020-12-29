77ca43a77ba38c066063747f2874d40eaad2c6f3
AdamKorcz <adam@adalogics.com>
2020-12-29 10:45:33 +0000

    Updated enhance fuzzer

--- prog/fuzzing/enhance_fuzzer.cc
+++ prog/fuzzing/enhance_fuzzer.cc
@@ -17,48 +17,37 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixContrastTRCMasked(NULL, pix_pointer_payload, NULL, 0.5);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixDarkenGray(NULL, pix_pointer_payload,  220,  10);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixEqualizeTRC(NULL, pix_pointer_payload,  220,  10);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixGammaTRCMasked(NULL, pix_pointer_payload, NULL,  1.0,  100,  175);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixGammaTRCWithAlpha(NULL, pix_pointer_payload, 0.5,  1.0,  100);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
+
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixHalfEdgeByBandpass(pix_pointer_payload, 2, 2, 4, 4);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	l_float32 sat;
@@ -69,50 +58,39 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixModifyBrightness(NULL, pix_pointer_payload, 0.5);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixModifyHue(NULL, pix_pointer_payload, 0.01 + 0.05 * 1);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixModifySaturation(NULL, pix_pointer_payload, -0.9 + 0.1 * 1);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
+
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixMosaicColorShiftRGB(pix_pointer_payload, -0.1, 0.0, 0.0, 0.0999, 1);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
+	
 
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	return_pix = pixMultConstantColor(pix_pointer_payload,  0.7,  0.4,  1.3);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 
 
 	kel = kernelCreate(3, 3);
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	pixMultMatrixColor( pix_pointer_payload, kel);
 	pixDestroy(&pix_pointer_payload);
-	if(return_pix!=NULL){
-		pixDestroy(&return_pix);
-	}
+	pixDestroy(&return_pix);
 	kernelDestroy(&kel);
-
 
 	
 	na1 = numaGammaTRC(1.0, 0, 255);
@@ -120,7 +98,8 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 	na3 = numaGammaTRC(1.0, 0, 255);
 	pix_pointer_payload = pixCopy(NULL, pixs_payload);
 	pix2 = pixMakeSymmetricMask(10, 10, 0.5, 0.5, L_USE_INNER);
-	pixTRCMapGeneral(pix_pointer_payload, pix2, na1, na2, na3);
+	return_pix = pixTRCMapGeneral(pix_pointer_payload, pix2, na1, na2, na3);
+	pixDestroy(&return_pix);
 	numaDestroy(&na1);
 	numaDestroy(&na2);
 	numaDestroy(&na3);
