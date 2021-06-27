DanBloomberg/leptonica

commit d15f1c24a16a65a4ae2e018a21ce9ad52b33ac64

Compiler complains about possible overflows in strncat. * Removed all instances, replacing with stringCat(). * Fixed stringCat() to do nothing in case of error Also, small changes for scaling resolution.

src/utils2.c
@@ -410,7 +410,8 @@ l_int32  i;
  *          rather than the number of bytes to copy), and does not complain
  *          if %src is null.
  *      (2) Never writes past end of dest.
- *      (3) If it can't append src (an error), it does nothing.
+ *      (3) If there is not enough room to append the src, which is an error,
+ *          it does nothing.
  *      (4) N.B. The order of 2nd and 3rd args is reversed from that in
  *          strncat, as in the Windows function strcat_s().
  * </pre>
@@ -438,7 +439,7 @@ l_int32  lendest, lensrc;
     lensrc = stringLength(src, size);
     if (lensrc == 0)
         return 0;
-    n = (lendest + lensrc > size - 1 ? size - lendest - 1 : lensrc);
+    n = (lendest + lensrc > size - 1 ? 0 : lensrc);
     if (n < 1)
         return ERROR_INT("dest too small for append", procName, -1);
 
@@ -527,9 +528,9 @@ l_int32  srclen1, srclen2, destlen;
         return (char *)ERROR_PTR("calloc fail for dest", procName, NULL);
 
     if (src1)
-        stringCopy(dest, src1, srclen1);
+        stringCat(dest, destlen, src1);
     if (src2)
-        strncat(dest, src2, srclen2);
+        stringCat(dest, destlen, src2);
     return dest;
 }
 
@@ -1588,7 +1589,7 @@ FILE  *fp;
         /* The 'b' flag to fopen() is ignored for all POSIX
          * conforming systems.  However, Windows needs the 'b' flag. */
     stringCopy(actualOperation, operation, 2);
-    strncat(actualOperation, "b", 2);
+    stringCat(actualOperation, 20, "b");
 
     if ((fp = fopenWriteStream(filename, actualOperation)) == NULL)
         return ERROR_INT("stream not opened", procName, 1);
@@ -2951,16 +2952,16 @@ size_t  len1, len2, len3, len4;
 
     len1 = strlen(basedir);
     len2 = strlen(subdirs);
-    len3 = len1 + len2 + 6;
-    if ((newdir = (char *)LEPT_CALLOC(len3 + 1, 1)) == NULL)
+    len3 = len1 + len2 + 8;
+    if ((newdir = (char *)LEPT_CALLOC(len3, 1)) == NULL)
         return (char *)ERROR_PTR("newdir not made", procName, NULL);
-    strncat(newdir, basedir, len3);  /* add basedir */
+    stringCat(newdir, len3, basedir);
     if (newdir[len1 - 1] != '/')  /* add '/' if necessary */
         newdir[len1] = '/';
     if (subdirs[0] == '/')  /* add subdirs, stripping leading '/' */
-        strncat(newdir, subdirs + 1, len3);
+        stringCat(newdir, len3, subdirs + 1);
     else
-        strncat(newdir, subdirs, len3);
+        stringCat(newdir, len3, subdirs);
     len4 = strlen(newdir);
     if (newdir[len4 - 1] == '/')  /* strip trailing '/' */
         newdir[len4 - 1] = '\0';
@@ -3057,7 +3058,8 @@ genPathname(const char  *dir,
 {
 l_int32  is_win32 = FALSE;
 char    *cdir, *pathout;
-l_int32  dirlen, namelen, size;
+l_int32  dirlen, namelen;
+size_t   size;
 
     PROCNAME("genPathname");
 
@@ -3122,7 +3124,7 @@ l_int32  dirlen, namelen, size;
     if (fname && strlen(fname) > 0) {
         dirlen = strlen(pathout);
         pathout[dirlen] = '/';
-        strncat(pathout, fname, namelen);
+        stringCat(pathout, size, fname);
     }
 
     LEPT_FREE(cdir);
@@ -3199,7 +3201,7 @@ size_t   pathlen;
 #endif  /*  ~ _WIN32 */
     pathlen = strlen(path);
     if (pathlen < nbytes - 1) {
-        strncpy(result, path, pathlen);
+        stringCat(result, nbytes, path);
     } else {
         L_ERROR("result array too small for path\n", procName);
         ret = 1;

src/pix1.c
@@ -1368,15 +1368,20 @@ pixScaleResolution(PIX       *pix,
                    l_float32  xscale,
                    l_float32  yscale)
 {
+l_float64  xres, yres;
+l_float64  maxres = 100000000.0;
+
     PROCNAME("pixScaleResolution");
 
     if (!pix)
         return ERROR_INT("pix not defined", procName, 1);
+    if (xscale <= 0 || yscale <= 0)
+        return ERROR_INT("invalid scaling ratio", procName, 1);
 
-    if (pix->xres != 0 && pix->yres != 0) {
-        pix->xres = (l_uint32)(xscale * (l_float32)(pix->xres) + 0.5);
-        pix->yres = (l_uint32)(yscale * (l_float32)(pix->yres) + 0.5);
-    }
+    xres = (l_float64)xscale * (l_float32)(pix->xres) + 0.5;
+    yres = (l_float64)yscale * (l_float32)(pix->yres) + 0.5;
+    pix->xres = (l_uint32)L_MIN(xres, maxres);
+    pix->yres = (l_uint32)L_MIN(yres, maxres);
     return 0;
 }
 

commit 05ff50e097cb3f65e4a8b603db2cbaf61336c98d

makeTempDirname: use confstr on macOS, and fix a bug in path generation
makeTempDirname() on iOS uses confstr() to get a temporary directory
because /tmp is not available to apps. macOS programs using the App
Sandbox are similarly restricted, and confstr is the recommended system
API to acquire a temporary directory path regardless.

This also fixes a bug introduced in
d15f1c24a16a65a4ae2e018a21ce9ad52b33ac64 where copying `path` to
`result` was changed from using strncpy() to stringCat(). Since the call
to confstr() already wrote to `result`, this led to the path being
incorrectly duplicated. Using stringCopy() resolves the issue.

src/utils2.c
@@ -200,7 +200,7 @@
 #include <sys/types.h>
 #endif
 
-#ifdef OS_IOS
+#ifdef __APPLE__
 #include <unistd.h>
 #include <errno.h>
 #endif
@@ -3296,7 +3296,7 @@ size_t   pathlen;
 
     memset(result, 0, nbytes);
 
-#ifdef OS_IOS
+#ifdef __APPLE__
     {
         size_t n = confstr(_CS_DARWIN_USER_TEMP_DIR, result, nbytes);
         if (n == 0) {
@@ -3309,7 +3309,7 @@ size_t   pathlen;
     }
 #else
     dir = pathJoin("/tmp", subdir);
-#endif /*  ~ OS_IOS */
+#endif /*  ~ __APPLE__ */
 
 #ifndef _WIN32
     path = stringNew(dir);
@@ -3318,7 +3318,7 @@ size_t   pathlen;
 #endif  /*  ~ _WIN32 */
     pathlen = strlen(path);
     if (pathlen < nbytes - 1) {
-        stringCat(result, nbytes, path);
+        stringCopy(result, path, nbytes);
     } else {
         L_ERROR("result array too small for path\n", procName);
         ret = 1;