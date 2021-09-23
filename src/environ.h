3b5d7d7f0dc839b69449bd7af064e1f994ee033e
Stefan Weil <sw@weilnetz.de>
2021-09-23 06:51:09 +0200

    Revert "Autoconf HAVE_FOO"
    This reverts commit 7a37c67b37d73df0a1b66a6d45b21816d4563c22.
    It was committed accidentally.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/environ.h
+++ src/environ.h
@@ -101,11 +101,7 @@ typedef uintptr_t l_uintptr_t;
  *  I/O libraries, plus zlib.  Setting any of these to 0 here causes
  *  non-functioning stubs to be linked.
  */
-#if defined(HAVE_CONFIG_H)
-
-# include <config_auto.h>
-
-#elif !defined(ANDROID_BUILD) && !defined(OS_IOS)
+#if !defined(HAVE_CONFIG_H) && !defined(ANDROID_BUILD) && !defined(OS_IOS)
 
   #if !defined(HAVE_LIBJPEG)
   #define  HAVE_LIBJPEG       1
@@ -118,6 +114,21 @@ typedef uintptr_t l_uintptr_t;
   #endif
   #if !defined(HAVE_LIBZ)
   #define  HAVE_LIBZ          1
+  #endif
+  #if !defined(HAVE_LIBGIF)
+  #define  HAVE_LIBGIF        0
+  #endif
+  #if !defined(HAVE_LIBUNGIF)
+  #define  HAVE_LIBUNGIF      0
+  #endif
+  #if !defined(HAVE_LIBWEBP)
+  #define  HAVE_LIBWEBP       0
+  #endif
+  #if !defined(HAVE_LIBWEBP_ANIM)
+  #define  HAVE_LIBWEBP_ANIM  0
+  #endif
+  #if !defined(HAVE_LIBJP2K)
+  #define  HAVE_LIBJP2K       0
   #endif
 
 
