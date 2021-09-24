5cbc4fbb020a93fc5f82e35bce51709fb144e3ed
danblooomberg <dan.bloomberg@gmail.com>
2021-09-24 12:58:15 -0600

    Increment version number 1.82.0 --> 1.83.0 in 6 files

--- sw.cpp
+++ sw.cpp
@@ -19,7 +19,7 @@ void build(Solution &s)
         t += "org.sw.demo.webmproject.webp"_dep;
     };
 
-    auto &leptonica = s.addTarget<LibraryTarget>("danbloomberg.leptonica", "1.82.0");
+    auto &leptonica = s.addTarget<LibraryTarget>("danbloomberg.leptonica", "1.83.0");
     leptonica += Git("https://github.com/DanBloomberg/leptonica");
 
     {
